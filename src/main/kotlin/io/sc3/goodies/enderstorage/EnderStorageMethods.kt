3636324d1807fbebf279641d98d7fb2411431cdd
Drew Edwards <github@drew.contact>
2023-01-08 14:43:09 +0000

    feat: more ender storage peripheral methods

--- src/main/kotlin/io/sc3/goodies/enderstorage/EnderStorageMethods.kt
+++ src/main/kotlin/io/sc3/goodies/enderstorage/EnderStorageMethods.kt
@@ -3,11 +3,12 @@ package io.sc3.goodies.enderstorage
 import dan200.computercraft.api.ComputerCraftAPI
 import dan200.computercraft.api.lua.LuaException
 import dan200.computercraft.api.lua.LuaFunction
+import dan200.computercraft.api.lua.MethodResult
 import dan200.computercraft.api.peripheral.GenericPeripheral
 import dan200.computercraft.api.peripheral.PeripheralType
+import io.sc3.goodies.ScGoodies.ModId
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.util.DyeColor
-import io.sc3.goodies.ScGoodies.ModId
 import kotlin.math.floor
 import kotlin.math.log2
 
@@ -19,20 +20,47 @@ object EnderStorageMethods : GenericPeripheral {
 
   @LuaFunction(mainThread = true)
   @JvmStatic
+  fun getFrequency(be: EnderStorageBlockEntity): MethodResult {
+    val freq = be.frequency
+    return MethodResult.of(freq.left.toInt(), freq.middle.toInt(), freq.right.toInt())
+  }
+
+  @LuaFunction(mainThread = true)
+  @JvmStatic
+  fun getOwner(be: EnderStorageBlockEntity): MethodResult {
+    val freq = be.frequency
+    return MethodResult.of(freq.owner?.toString(), freq.ownerName)
+  }
+
+  @LuaFunction(mainThread = true)
+  @JvmStatic
+  fun isPersonal(be: EnderStorageBlockEntity): Boolean =
+    be.frequency.personal
+
+  @LuaFunction(mainThread = true)
+  @JvmStatic
+  fun areComputerChangesEnabled(be: EnderStorageBlockEntity): Boolean =
+    be.computerChangesEnabled
+
+  @LuaFunction(mainThread = true)
+  @JvmStatic
   fun setFrequency(be: EnderStorageBlockEntity, left: Int, middle: Int, right: Int) {
     if (be.frequency.personal && !be.computerChangesEnabled) {
       throw LuaException("Computer changes are disabled")
     }
 
     be.frequency = be.frequency.copy(
-      left = intToColor(left),
-      middle = intToColor(middle),
-      right = intToColor(right)
+      left   = left.toColor(),
+      middle = middle.toColor(),
+      right  = right.toColor()
     )
   }
 
-  private fun intToColor(n: Int): DyeColor =
-    DyeColor.byId(floor(log2(n.toDouble())).toInt())
+  private fun Int.toColor(): DyeColor =
+    DyeColor.byId(floor(log2(toDouble())).toInt())
+
+  private fun DyeColor.toInt(): Int =
+    1 shl id
 
   internal fun register() {
     if (FabricLoader.getInstance().isModLoaded("computercraft")) {

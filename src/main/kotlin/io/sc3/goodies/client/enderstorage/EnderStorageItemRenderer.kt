41ae12d2c5f74bbdc8205854273da7c8f6518d3c
Drew Edwards <github@drew.contact>
2023-01-28 20:37:41 +0000

    fix: emerald in Ender Storage item (fixes #6)

--- src/main/kotlin/io/sc3/goodies/client/enderstorage/EnderStorageItemRenderer.kt
+++ src/main/kotlin/io/sc3/goodies/client/enderstorage/EnderStorageItemRenderer.kt
@@ -1,13 +1,13 @@
 package io.sc3.goodies.client.enderstorage
 
+import io.sc3.goodies.client.enderstorage.EnderStorageBlockEntityRenderer.Companion.renderChest
+import io.sc3.goodies.enderstorage.Frequency
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry.DynamicItemRenderer
 import net.minecraft.client.render.VertexConsumerProvider
 import net.minecraft.client.render.model.json.ModelTransformation
 import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.item.ItemStack
 import net.minecraft.util.math.Direction
-import io.sc3.goodies.client.enderstorage.EnderStorageBlockEntityRenderer.Companion.renderChest
-import io.sc3.goodies.enderstorage.Frequency
 
 object EnderStorageItemRenderer : DynamicItemRenderer {
   private val defaultFrequency = Frequency()
@@ -15,6 +15,7 @@ object EnderStorageItemRenderer : DynamicItemRenderer {
   override fun render(stack: ItemStack, mode: ModelTransformation.Mode, matrices: MatrixStack,
                       vertexConsumers: VertexConsumerProvider, light: Int, overlay: Int) {
     val frequency = Frequency.fromStack(stack) ?: defaultFrequency
-    renderChest(matrices, vertexConsumers, Direction.NORTH, 0.0f, frequency, false, light, overlay)
+    val changesEnabled = stack.getSubNbt("BlockEntityTag")?.getBoolean("computerChangesEnabled") ?: false
+    renderChest(matrices, vertexConsumers, Direction.NORTH, 0.0f, frequency, changesEnabled, light, overlay)
   }
 }

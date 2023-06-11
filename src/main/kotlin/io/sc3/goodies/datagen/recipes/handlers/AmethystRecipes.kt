c944fba10061f34e6ebb2e4cb17da61ac16b0c49
Marcus <accounts@znepb.me>
2023-06-11 14:28:19 -0400

    feat: stonecutter recipies for concrete & amethyst (#37)
    * feat: stonecutter recipies for concrete & amethyst
    
    * feat: better way of generating stonecutter recipie

--- src/main/kotlin/io/sc3/goodies/datagen/recipes/handlers/AmethystRecipes.kt
+++ src/main/kotlin/io/sc3/goodies/datagen/recipes/handlers/AmethystRecipes.kt
@@ -3,7 +3,7 @@ package io.sc3.goodies.datagen.recipes.handlers
 import io.sc3.goodies.misc.AmethystExtras
 import io.sc3.library.recipe.RecipeHandler
 import net.minecraft.data.server.recipe.RecipeJsonProvider
-import net.minecraft.data.server.recipe.RecipeProvider
+import net.minecraft.data.server.recipe.RecipeProvider.*
 import net.minecraft.recipe.Ingredient
 import net.minecraft.recipe.book.RecipeCategory
 import java.util.function.Consumer
@@ -11,12 +11,15 @@ import java.util.function.Consumer
 object AmethystRecipes : RecipeHandler {
   override fun generateRecipes(exporter: Consumer<RecipeJsonProvider>) {
     // Amethyst Slabs and Stairs
-    RecipeProvider.createSlabRecipe(RecipeCategory.BUILDING_BLOCKS, AmethystExtras.slabBlock, Ingredient.ofItems(AmethystExtras.baseBlock))
-      .criterion("items", RecipeProvider.conditionsFromItem(AmethystExtras.baseBlock))
+    createSlabRecipe(RecipeCategory.BUILDING_BLOCKS, AmethystExtras.slabBlock, Ingredient.ofItems(AmethystExtras.baseBlock))
+      .criterion("items", conditionsFromItem(AmethystExtras.baseBlock))
       .offerTo(exporter)
 
-    RecipeProvider.createStairsRecipe(AmethystExtras.stairsBlock, Ingredient.ofItems(AmethystExtras.baseBlock))
-      .criterion("items", RecipeProvider.conditionsFromItem(AmethystExtras.baseBlock))
+    createStairsRecipe(AmethystExtras.stairsBlock, Ingredient.ofItems(AmethystExtras.baseBlock))
+      .criterion("items", conditionsFromItem(AmethystExtras.baseBlock))
       .offerTo(exporter)
+
+    offerStonecuttingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, AmethystExtras.slabBlock, AmethystExtras.baseBlock, 2)
+    offerStonecuttingRecipe(exporter, RecipeCategory.BUILDING_BLOCKS, AmethystExtras.stairsBlock, AmethystExtras.baseBlock, 1)
   }
 }

b659c9035d8e27994d2f385cfeec8458bf00c6ff
hugeblank <hugeblank@gmail.com>
2024-06-29 17:35:45 -0700

    implement requested changes

--- src/main/kotlin/io/sc3/goodies/misc/StairWrenchItem.kt
+++ src/main/kotlin/io/sc3/goodies/misc/StairWrenchItem.kt
@@ -1,16 +1,14 @@
 package io.sc3.goodies.misc
 
-import io.sc3.goodies.itemmagnet.ItemMagnetItem.Companion.radius
 import io.sc3.goodies.util.BaseItem
-import io.sc3.library.Tooltips
 import io.sc3.text.color
+import net.minecraft.block.Block
 import net.minecraft.block.BlockState
 import net.minecraft.block.StairsBlock
 import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.ItemUsageContext
-import net.minecraft.registry.Registries
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.state.property.Property
 import net.minecraft.text.Text
@@ -24,11 +22,11 @@ import net.minecraft.world.World
 import net.minecraft.world.WorldAccess
 
 class StairWrenchItem(settings: Settings) : BaseItem(settings) {
-  private var properties = listOf(StairsBlock.FACING, StairsBlock.HALF, StairsBlock.SHAPE) as List<Property<*>>
+  private var properties: List<Property<*>> = listOf(StairsBlock.FACING, StairsBlock.HALF, StairsBlock.SHAPE)
 
   override fun canMine(state: BlockState?, world: World, pos: BlockPos?, miner: PlayerEntity): Boolean {
     if (!world.isClient) {
-      this.use(miner, state!!, world, pos!!, false, miner.getStackInHand(Hand.MAIN_HAND))
+      this.use(miner, state ?: return false, world, pos ?: return false, false, miner.getStackInHand(Hand.MAIN_HAND))
     }
     return false
   }
@@ -61,18 +59,14 @@ class StairWrenchItem(settings: Settings) : BaseItem(settings) {
     if (block is StairsBlock) {
       val stateManager = block.stateManager
       val mode = stack.orCreateNbt.getString("Property")
-      var property = stateManager.getProperty(mode)
+      val property = stateManager.getProperty(mode) ?: properties.first()
       if (update) {
-        if (property == null) {
-          property = properties.iterator().next()
-        }
         val blockState = cycle(state, property, player.shouldCancelInteraction())
-        world.setBlockState(pos, blockState, 18)
+        world.setBlockState(pos, blockState, Block.FORCE_STATE or Block.NOTIFY_LISTENERS)
       } else {
-        property = cycle(properties, property, player.shouldCancelInteraction()) as Property<*>
-        val name = property.name
-        stack.orCreateNbt.putString("Property", name)
-        sendMessage(player,translatable("$translationKey.mode", name))
+        val newProperty = cycle(properties, property, player.shouldCancelInteraction())
+        stack.orCreateNbt.putString("Property", newProperty.name)
+        sendMessage(player as ServerPlayerEntity, translatable("$translationKey.mode", newProperty.name))
       }
       return true
     }
@@ -88,7 +82,7 @@ class StairWrenchItem(settings: Settings) : BaseItem(settings) {
   }
 
 
-  private fun sendMessage(player: PlayerEntity, message: Text) {
-    (player as ServerPlayerEntity).sendMessageToClient(message, true)
+  private fun sendMessage(player: ServerPlayerEntity, message: Text) {
+    player.sendMessageToClient(message, true)
   }
 }

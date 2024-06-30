78aa9c6275d761c43d3ca5a807668f6ba7d15a4e
Drew Edwards <github@drew.contact>
2024-06-30 18:20:00 +0100

    fix: don't mutate nbt in appendTooltip

--- src/main/kotlin/io/sc3/goodies/misc/StairWrenchItem.kt
+++ src/main/kotlin/io/sc3/goodies/misc/StairWrenchItem.kt
@@ -1,6 +1,7 @@
 package io.sc3.goodies.misc
 
 import io.sc3.goodies.util.BaseItem
+import io.sc3.library.ext.optString
 import io.sc3.text.color
 import net.minecraft.block.Block.FORCE_STATE
 import net.minecraft.block.Block.NOTIFY_LISTENERS
@@ -16,8 +17,8 @@ import net.minecraft.state.property.Property
 import net.minecraft.text.Text
 import net.minecraft.text.Text.translatable
 import net.minecraft.util.ActionResult
-import net.minecraft.util.Formatting
-import net.minecraft.util.Hand
+import net.minecraft.util.Formatting.GRAY
+import net.minecraft.util.Hand.MAIN_HAND
 import net.minecraft.util.Util
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
@@ -25,25 +26,21 @@ import net.minecraft.world.World
 import net.minecraft.world.WorldAccess
 
 class StairWrenchItem(settings: Settings) : BaseItem(settings) {
-  private var properties = listOf(StairsBlock.FACING, StairsBlock.HALF, StairsBlock.SHAPE)
+  private val properties = listOf(StairsBlock.FACING, StairsBlock.HALF, StairsBlock.SHAPE)
+  private val fallbackProperty = properties.first()
 
   override fun canMine(state: BlockState, world: World, pos: BlockPos, miner: PlayerEntity): Boolean {
     if (!world.isClient) {
-      crank(miner, state, world, pos, false, miner.getStackInHand(Hand.MAIN_HAND))
+      crank(miner, state, world, pos, false, miner.getStackInHand(MAIN_HAND))
     }
 
     return false
   }
 
   override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
-    var mode = stack.orCreateNbt.getString("Property")
-    if (mode.isEmpty()) {
-      // Fresh item (crafting result), REI, etc. Show the first property as the default mode
-      stack.nbt?.putString("Property", StairsBlock.FACING.name)
-      mode = StairsBlock.FACING.name
-    }
-
-    tooltip.add(translatable("$translationKey.mode", mode).color(Formatting.GRAY))
+    // For a fresh item (crafting result, REI, etc.), show the first property as the default mode
+    val mode = stack.orCreateNbt.optString("Property") ?: fallbackProperty.name
+    tooltip.add(translatable("$translationKey.mode", mode).color(GRAY))
     super.appendTooltip(stack, world, tooltip, context)
   }
 
@@ -68,8 +65,8 @@ class StairWrenchItem(settings: Settings) : BaseItem(settings) {
     // TODO: Allow this to work with some other types of blocks
     if (!state.isIn(BlockTags.STAIRS)) return false
 
-    val mode = stack.orCreateNbt.getString("Property")
-    val property = state.block.stateManager.getProperty(mode)
+    val mode = stack.orCreateNbt.optString("Property")
+    val property = mode?.let { state.block.stateManager.getProperty(mode) } ?: fallbackProperty
     if (property == null || !properties.contains(property)) {
       return false // Invalid mode (target block doesn't have the property)
     }
@@ -79,7 +76,7 @@ class StairWrenchItem(settings: Settings) : BaseItem(settings) {
       val newProperty = cycle(state, property, player.shouldCancelInteraction())
       world.setBlockState(pos, newProperty, FORCE_STATE or NOTIFY_LISTENERS)
     } else {
-      // Cycle the property mode on the item and save it
+      // Cycle the property mode and save it to the item
       val newProperty = cycle(properties, property, player.shouldCancelInteraction())
       stack.orCreateNbt.putString("Property", newProperty.name)
 
